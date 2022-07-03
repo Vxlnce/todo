a5988da0e73d7f4ba4083218b771b11655991940
Charlie <30825964+Vxlnce@users.noreply.github.com>
2022-07-03 15:09:16 +1000

    Made a table to hold completed tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import { TodoBanner } from "./todoBanner";
+import {TodoBanner} from "./TodoBanner";
 import { TodoCreator } from "./TodoCreator";
 import { TodoRow } from "./TodoRow";
+import {VisibilityControl} from "./VisibilityControl";
 export default class App extends Component<any, any> {
     constructor(props: any) {
         super(props);
         this.state = {
-            userName: "Adam",
+            userName: "Charlie",
             todoItems: [{ action: "Buy Flowers", done: false },
                 { action: "Get Shoes", done: false },
                 { action: "Collect Tickets", done: true },
                 { action: "Call Joe", done: false }],
-            //newItemText: ""
+            showCompleted: true
         }
     }
+
     updateNewTextValue = (event: any) => {
         this.setState({ newItemText: event.target.value });
     }
+
     createNewTodo = (task: any) => {
         if (!this.state.todoItems.find((item: any) => item.action === task)) {
             this.setState({
@@ -24,11 +27,14 @@ export default class App extends Component<any, any> {
             });
         }
     }
+
     toggleTodo = (todo: any) => this.setState({ todoItems:
             this.state.todoItems.map((item: any) => item.action === todo.action
                 ? { ...item, done: !item.done } : item) });
-    todoTableRows = () => this.state.todoItems.map((item: any) =>
-        <TodoRow key={ item.action } item={ item } callback={ this.toggleTodo } />)
+
+    todoTableRows = (doneValue: any) => this.state.todoItems.filter((item: any) => item.done === doneValue)
+        .map((item: any) => <TodoRow key={ item.action } item={ item } callback={ this.toggleTodo } />)
+
     render = () =>
         <div>
             <TodoBanner name={ this.state.userName } tasks={this.state.todoItems } />
@@ -38,8 +44,21 @@ export default class App extends Component<any, any> {
                     <thead>
                     <tr><th>Description</th><th>Done</th></tr>
                     </thead>
-                    <tbody>{ this.todoTableRows() }</tbody>
+                    <tbody>{ this.todoTableRows(false) }</tbody>
                 </table>
+                <div className="bg-secondary text-white text-center p-2">
+                    <VisibilityControl description="Completed Tasks" isChecked={this.state.showCompleted}
+                                       callback={(checked: any) => this.setState({showCompleted: checked})}/>
+                </div>
+                {
+                    this.state.showCompleted &&
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                        <tr><th>Description</th><th>Done</th></tr>
+                        </thead>
+                        <tbody>{this.todoTableRows(true)}</tbody>
+                    </table>
+                }
             </div>
         </div>
 }
---
+++ src/VisibilityControl.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export class VisibilityControl extends Component<any, any> {
+    render = () =>
+        <div className="form-check">
+            <input type="checkbox" className="form-check-input" checked={this.props.isChecked} onChange={(e) => this.props.callback(e.target.checked)}/>
+            <label className="form-check-label">Show {this.props.description}</label>
+        </div>
+}
